6d5bf6b1ad3897e0081fbe1557ccad661fd0ca0e
Samuel Howell <rugnirtheviking@gmail.com>
2022-06-16 23:40:38 +0200

    begone evil spirits

--- engine.py
+++ engine.py
@@ -10,8 +10,16 @@ from pygame.ftfont import Font
 
 from colors import WHITE
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 class Engine:
+    # some magic to ward away evil spirits
+    __metaclass__ = Singleton
     textRect: pygame.rect
     smallfont: Union[Font, FontType]
     titletext: pygame.Surface
